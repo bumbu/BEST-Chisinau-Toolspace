8e8a207b925bacb6cd26210afaaf7347e29fde87
bumbu <bmbalex@gmail.com>
2012-07-14 05:12:00 +0300

    Aditional sign for title

--- classes/request.php
+++ classes/request.php
@@ -57,7 +57,7 @@ class Request{
 				
 			// System specific
 			case 'title':
-				return preg_replace("/[^\w0-9\s\-\_\[\]]/u", "", $value);
+				return preg_replace("/[^\w0-9\s\-\_\[\]\!]/u", "", $value);
 				break;
 			case 'tags':
 				return preg_replace("/[^A-Za-z0-9\s\[\]\-\_]/u", "", $value);
